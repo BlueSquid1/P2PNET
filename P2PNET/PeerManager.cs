97d6116049a36d931e3ae5bfe15e20aeb4d01149
Clinton Page <clinton.page123@gmail.com>
2016-12-29 15:28:05 +1030

    inital commit

---
+++ P2PNET/PeerManager.cs
@@ -0,0 +1,33 @@
+﻿using Sockets.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P2PNET
+{
+    public class PeerManager
+    {
+        public event EventHandler<PeerUpdateEventArgs> PeerChange;
+        public event EventHandler<KeyStrokeEventArgs> msgReceived;
+
+        public int PortNum { get; }
+
+        public void SendMsgAsyncTCP(string ipAddress, byte[] msg)
+        {
+
+        }
+
+        public void SendMsgAsyncUDP(string ipAddress, byte[] msg)
+        {
+
+        }
+
+        public void SendBroadcast(byte[] msg)
+        {
+
+        }
+
+
+    }
+}
